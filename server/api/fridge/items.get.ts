83ce07c4c44488a2cdb79b8737d9f40395ae022b
Hamza Ghaffari <64998505+Aptenodyte@users.noreply.github.com>
2025-11-09 12:14:08 -0600

    Move pages/ to the app/ directory instead of project root

--- server/api/fridge/items.get.ts
+++ server/api/fridge/items.get.ts
@@ -1,7 +1,7 @@
-import prisma from "~/generated/prisma/client";
+// import prisma from "../../utils/prisma";
 
-export default defineEventHandler(async (event) => {
-    const fridge = await prisma.fridge.findUnique({ where: {
-        userId: 
-    }})
-})
+// export default defineEventHandler(async (event) => {
+//     const fridge = await prisma.fridge.findUnique({ where: {
+//         userId: 
+//     }})
+// })
